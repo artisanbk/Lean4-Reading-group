16b6361b95b6b4a4d38b9866a8721ec52e598b0c
bk. <62876764+artisanbk@users.noreply.github.com>
2026-09-15 20:13:57 +0800

    some chapter 2 stuff

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -150,24 +150,44 @@ example {c : ℚ} (h1 : 4 * c + 1 = 3 * c - 2) : c = -3 :=
     _ = -3 := by ring
 
 example {p : ℝ} (h1 : 5 * p - 3 = 3 * p + 1) : p = 2 :=
-  sorry
+  calc
+    p = (5 * p - 3 - (3 * p - 3)) / 2 := by ring
+    _ = (3 * p + 1 - (3 * p - 3)) / 2 := by rw[h1]
+    _ = 2 := by ring
 
 example {x y : ℤ} (h1 : 2 * x + y = 4) (h2 : x + y = 1) : x = 3 :=
-  sorry
+  calc x = (2 * x + y) - (x + y) := by ring
+  _ = 4 - 1 := by rw[h1, h2]
+  _ = 3 := by ring
 
 example {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 :=
-  sorry
+  calc a = ((a + 2 * b) + 2 * (a - b)) / 3 := by ring
+  _ = (4 + 2 * 1) / 3 := by rw[h1, h2]
+  _ = 2 := by ring
 
 example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 :=
-  sorry
+  calc u ^ 2 + 3 * u + 1 = (u + 1) ^ 2 + u := by ring
+  _ = v^2 + u := by rw[h1]
+  _ = v^2 + (u + 1) - 1 := by ring
+  _ = v ^ 2 + v - 1 := by rw[h1]
 
 example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 :=
-  sorry
+  calc
+    t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 =
+      (t^2 - 4) * (t ^ 2 + 3 * t + 1) + (10 * t + 2) := by ring
+    _ = 0 * (t ^ 2 + 3 * t + 1) + (10 * t + 2) := by rw[ht]
+    _ = 10 * t + 2 := by ring
 
 example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 :=
-  sorry
+  calc
+    y = y + (2 - y) * (x + 3 - 3) / 2 - (2 * x - y * x) / 2 := by ring
+    _ = y + (2 - y) * (5 - 3) / 2 - 0 / 2 := by rw[h1,h2]
+    _ = 2 := by ring
 
 example {p q r : ℚ} (h1 : p + q + r = 0) (h2 : p * q + p * r + q * r = 2) :
     p ^ 2 + q ^ 2 + r ^ 2 = -4 :=
-  sorry
+  calc
+    p ^ 2 + q ^ 2 + r ^ 2 = (p + q + r) ^ 2 - 2 * (p * q + p * r + q * r) := by ring
+    _ = 0 ^ 2 - 2 * 2 := by rw[h1,h2]
+    _ = -4 := by ring
--- Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
+++ Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
@@ -12,7 +12,10 @@ example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
     _ < 1 := by numbers
 
 example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
-  sorry
+  apply ne_of_gt
+  calc
+    y ^ 2 + 1 ≥ 1 := by extra
+    _ > 0 := by numbers
 
 example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
   apply le_antisymm
@@ -26,7 +29,13 @@ example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
 
 
 example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by
-  sorry
+  apply ne_of_gt
+  calc
+    3 * m = 3 * (m + 1 - 1) := by ring
+    _ = 3 * (5 - 1) := by rw[hm]
+    _ = 12 := by ring
+    _ > 6 := by numbers
 
 example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
+  apply le_antisymm
   sorry
